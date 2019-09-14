4e33f30f26cc31300e2739021be3354f4fc73f0c
Bartek Kawik <bartek.kawik@gmail.com>
2019-09-14 19:06:58 -0400

    Adding data access

---
+++ WiredBrainCoffe.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,8 @@
+﻿namespace WiredBrainCoffe.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int? BeansInStockInKg { get; set; }
+    }
+}
---
+++ WiredBrainCoffe.DataAccess/Model/CoffeeShopDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WiredBrainCoffe.DataAccess.Model
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Palatka", BeansInStockInKg = 100 };
+            yield return new CoffeeShop { Location = "Orlando", BeansInStockInKg = 50 };
+            yield return new CoffeeShop { Location = "Miami", BeansInStockInKg = 75 };
+        }
+    }
+}
