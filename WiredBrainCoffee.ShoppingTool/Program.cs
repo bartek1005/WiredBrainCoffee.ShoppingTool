3f24206b65944aa4320ae715028108448873d3b5
Bartek Kawik <bartek.kawik@gmail.com>
2019-09-14 19:07:12 -0400

    Adding help command

--- WiredBrainCoffee.ShoppingTool/Program.cs
+++ WiredBrainCoffee.ShoppingTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WiredBrainCoffe.DataAccess.Model;
 
 namespace WiredBrainCoffee.ShoppingTool
 {
@@ -7,6 +8,25 @@ namespace WiredBrainCoffee.ShoppingTool
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Write 'help' to display data");
+
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee shops");
+                    foreach (var c in coffeeShops)
+                    {
+                        Console.WriteLine($"> " + c.Location);
+                    }
+                }
+
+            }
         }
     }
 }
