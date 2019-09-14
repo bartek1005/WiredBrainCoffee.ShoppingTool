2beea2e08ec962f8138830298bf942bacb4e3094
Bartek Kawik <bartek.kawik@gmail.com>
2019-09-14 19:27:42 -0400

    Implement quit command

--- WiredBrainCoffee.ShoppingTool/Program.cs
+++ WiredBrainCoffee.ShoppingTool/Program.cs
@@ -8,13 +8,17 @@ namespace WiredBrainCoffee.ShoppingTool
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Write 'help' to display data");
+            Console.WriteLine("Write 'help' to display data," + " write 'quit' to close application");
 
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
 
             while (true)
             {
                 var line = Console.ReadLine();
+
+                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                    break;
+
                 var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
 
                 if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
