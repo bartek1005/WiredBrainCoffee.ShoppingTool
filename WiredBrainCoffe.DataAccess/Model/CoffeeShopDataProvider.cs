bea86b627be15c7e2e505260f314010afccd28ce
Bartek Kawik <bartek.kawik@gmail.com>
2019-09-15 21:46:49 -0400

    Paper cups in stock data

--- WiredBrainCoffe.DataAccess/Model/CoffeeShopDataProvider.cs
+++ WiredBrainCoffe.DataAccess/Model/CoffeeShopDataProvider.cs
@@ -8,10 +8,10 @@ namespace WiredBrainCoffe.DataAccess.Model
     {
         public IEnumerable<CoffeeShop> LoadCoffeeShops()
         {
-            yield return new CoffeeShop { Location = "Palatka", BeansInStockInKg = 100 };
-            yield return new CoffeeShop { Location = "Orlando", BeansInStockInKg = 50 };
-            yield return new CoffeeShop { Location = "Miami", BeansInStockInKg = 75 };
-            yield return new CoffeeShop { Location = "Ocala", BeansInStockInKg = 10 };
+            yield return new CoffeeShop { Location = "Palatka", BeansInStockInKg = 100, PaperCupsInStock=100 };
+            yield return new CoffeeShop { Location = "Orlando", BeansInStockInKg = 50, PaperCupsInStock = 800 };
+            yield return new CoffeeShop { Location = "Miami", BeansInStockInKg = 75, PaperCupsInStock = 123 };
+            yield return new CoffeeShop { Location = "Ocala", BeansInStockInKg = 10, PaperCupsInStock = 23 };
 
         }
     }
